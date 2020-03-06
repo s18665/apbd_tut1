b4f3f41a45081700360d167fede464bd6c5f4280
Piotr Zaborowski <s18665@pjwstk.edu.pl>
2020-03-06 15:00:15 +0100

    FC

---
+++ Tutorial1_Solution/Tutorial1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Tutorial1
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var websiteUrl = args[0];
+            var httpClient = new HttpClient();
+
+            var respones = await httpClient.GetAsync(websiteUrl);
+
+            if (respones.IsSuccessStatusCode)
+            {
+                var htmlContent =await respones.Content.ReadAsStringAsync();
+
+                var regex = new Regex("[a-z0-9]+[a-z0-9]*@[a-z]+\\.[a-z]+", RegexOptions.IgnoreCase);
+
+                var emialAdresses = regex.Matches(htmlContent);
+
+                foreach (var match in emialAdresses)
+                {
+                    Console.WriteLine(match.ToString());
+                }
+
+            }
+
+
+            
+    }
+    }
+}
